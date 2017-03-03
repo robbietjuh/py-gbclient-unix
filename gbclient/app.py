1cb0440a0c1285c53301aa862f896f6c11e99e41
Robert de Vries <r.devries@robbytu.net>
2017-03-03 03:18:50 +0100

    + Added support for CORS

--- gbclient/app.py
+++ gbclient/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from settings import *
 
 app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
 app.config.from_pyfile('settings.py', silent=True)
 
+if CORS_ENABLED:
+    cors = CORS(app, **CORS_CONFIG)
 
 @app.route('/')
 def index():
--- gbclient/settings.py
+++ gbclient/settings.py
@@ -1,9 +1,15 @@
 import os
-
-DEBUG = True
 SETTINGS_PATH = os.path.abspath(__file__)
 PARENT_DIR = os.path.dirname(os.path.dirname(SETTINGS_PATH))
+
+DEBUG = True
 TEMPLATE_FOLDER = os.path.join(PARENT_DIR, 'examples')
+
+# Docs: http://flask-cors.corydolphin.com/en/latest/api.html
+CORS_ENABLED = False
+CORS_CONFIG = {
+    'origins': ['localhost',],
+}
 
 try:
     from local_settings import *
