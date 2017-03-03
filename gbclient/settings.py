6774df243254cf24b56e326bc3438b5200b8e2b2
Robert de Vries <r.devries@robbytu.net>
2017-03-03 02:42:11 +0100

    * Use absolute path for setting up the TEMPLATE_FOLDER setting

--- gbclient/settings.py
+++ gbclient/settings.py
@@ -1,7 +1,8 @@
 import os
 
 DEBUG = True
-PARENT_DIR = os.path.dirname(os.path.dirname(__file__))
+SETTINGS_PATH = os.path.abspath(__file__)
+PARENT_DIR = os.path.dirname(os.path.dirname(SETTINGS_PATH))
 TEMPLATE_FOLDER = os.path.join(PARENT_DIR, 'examples')
 
 try:
